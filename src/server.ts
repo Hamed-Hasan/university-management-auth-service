3e7acb45c55ac4a0dbefe30f9f416053c65b1dd7
John Doe <hamedhasan422421@gmail.com>
2023-09-23 08:07:38 +0300

    🚀 Update code to connect to database and start server
    Refactored the code to improve database connection handling and server startup. Added error logging for uncaught exceptions and unhandled rejections. Also, connected to Redis for event subscription.

---
+++ src/server.ts
@@ -0,0 +1,55 @@
+/* eslint-disable no-console */
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import subscribeToEvents from './app/events';
+import config from './config/index';
+import { errorlogger } from './shared/logger';
+import { RedisClient } from './shared/redis';
+
+process.on('uncaughtException', error => {
+  errorlogger.error(error);
+  process.exit(1);
+});
+
+let server: Server;
+
+async function bootstrap() {
+  try {
+    await RedisClient.connect().then(() => {
+      subscribeToEvents()
+    });
+
+
+    await mongoose.connect(config.database_url as string);
+    // logger.info(`🛢   Database is connected successfully`);
+    console.log(`🛢   Auth Service connected successfully`);
+
+    server = app.listen(config.port, () => {
+      // logger.info(`Application  listening on port ${config.port}`);
+      console.log(`Application  listening on port ${config.port}`);
+    });
+  } catch (err) {
+    errorlogger.error('Failed to connect database', err);
+  }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+bootstrap();
+
+// process.on('SIGTERM', () => {
+//   logger.info('SIGTERM is received');
+//   if (server) {
+//     server.close();
+//   }
+// });
