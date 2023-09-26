e9c19d4b88314e50c912c7c4460c4727b120382f
John Doe <hamedhasan422421@gmail.com>
2023-09-26 12:18:25 +0300

    🚀 Added createFaculty function ✨ Implemented getSingleFaculty endpoint 🔄 Refactored getAllFaculties route 🆕 Added createFaculty and getSingleFaculty functions 📚 Updated AcademicFacultyService methods

---
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { academicFacultyFilterableFields } from './academicFaculty.constants';
+import { IAcademicFaculty } from './academicFaculty.interfaces';
+import { AcademicFacultyService } from './academicFaculty.service';
+
+const createFaculty = catchAsync(async (req: Request, res: Response) => {
+  const { ...academicFacultyData } = req.body;
+  const result = await AcademicFacultyService.createFaculty(
+    academicFacultyData
+  );
+  sendResponse<IAcademicFaculty>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty created successfully',
+    data: result,
+  });
+});
+
+const getSingleFaculty = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await AcademicFacultyService.getSingleFaculty(id);
+
+  sendResponse<IAcademicFaculty>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty fetched successfully',
+    data: result,
+  });
+});
+
+const getAllFaculties = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, academicFacultyFilterableFields);
+  const paginationOptions = pick(req.query, paginationFields);
+
+  const result = await AcademicFacultyService.getAllFaculties(
+    filters,
+    paginationOptions
+  );
+
+  sendResponse<IAcademicFaculty[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculties fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
