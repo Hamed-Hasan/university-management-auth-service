79d47ba6a85a1269198601b00cac3b0c69eee650
John Doe <hamedhasan422421@gmail.com>
2023-09-27 13:59:03 +0300

    🚀 Updated Academic Semester 📚 🗑️ Deleted Academic Semester Successfully! 🎓 📝 Updated Academic Semester Data 📊 📚 Created New Academic Semester 🌟

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -52,3 +52,37 @@ const getAllSemesters = catchAsync(async (req: Request, res: Response) => {
     data: result.data,
   });
 });
+
+const updateSemester = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const updatedData = req.body;
+
+  const result = await AcademicSemesterService.updateSemester(id, updatedData);
+
+  sendResponse<IAcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester updated successfully !',
+    data: result,
+  });
+});
+const deleteSemester = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const result = await AcademicSemesterService.deleteSemester(id);
+
+  sendResponse<IAcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester deleted successfully !',
+    data: result,
+  });
+});
+
+export const AcademicSemesterController = {
+  createSemester,
+  getSingleSemester,
+  getAllSemesters,
+  updateSemester,
+  deleteSemester,
+};
