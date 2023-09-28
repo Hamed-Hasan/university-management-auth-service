abcd7d21b75c86ca5d4c5afa0bcdc883f34ab8c5
John Doe <hamedhasan422421@gmail.com>
2023-09-28 14:55:49 +0300

    🐛 Fix: Updated the updateAdmin function to properly handle name updates. 🚀 🚧 Work in Progress: Implementing the deleteAdmin function. Cleaning up code. 🧹

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -74,3 +74,66 @@ const getAllAdmins = async (
     data: result,
   };
 };
+
+const updateAdmin = async (
+  id: string,
+  payload: Partial<IAdmin>
+): Promise<IAdmin | null> => {
+  const isExist = await Admin.findOne({ id });
+
+  if (!isExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Admin not found !');
+  }
+
+  const { name, ...adminData } = payload;
+
+  const updatedStudentData: Partial<IAdmin> = { ...adminData };
+
+  if (name && Object.keys(name).length > 0) {
+    Object.keys(name).forEach(key => {
+      const nameKey = `name.${key}` as keyof Partial<IAdmin>;
+      (updatedStudentData as any)[nameKey] = name[key as keyof typeof name];
+    });
+  }
+
+  const result = await Admin.findOneAndUpdate({ id }, updatedStudentData, {
+    new: true,
+  });
+  return result;
+};
+
+const deleteAdmin = async (id: string): Promise<IAdmin | null> => {
+  // check if the faculty is exist
+  const isExist = await Admin.findOne({ id });
+
+  if (!isExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Faculty not found !');
+  }
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+    //delete student first
+    const student = await Admin.findOneAndDelete({ id }, { session });
+    if (!student) {
+      throw new ApiError(404, 'Failed to delete student');
+    }
+    //delete user
+    await User.deleteOne({ id });
+    session.commitTransaction();
+    session.endSession();
+
+    return student;
+  } catch (error) {
+    session.abortTransaction();
+    throw error;
+  }
+};
+
+export const AdminService = {
+  getSingleAdmin,
+  getAllAdmins,
+  updateAdmin,
+  deleteAdmin,
+};
