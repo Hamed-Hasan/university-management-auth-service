3b2757fddf742d739524d6544510d59610568817
John Doe <hamedhasan422421@gmail.com>
2023-10-05 22:52:23 +0300

    Add validation for faculty email field 📧..... Fix issue with faculty search functionality 🚀...... Update academic faculty field 📚....... Refactor faculty contact number handling 📞...... Improve faculty gender filter logic 🧑‍🤝‍🧑...... Implement emergency contact number validation 🚑.......

---
+++ src/app/modules/faculty/faculty.constant.ts
@@ -0,0 +1,23 @@
+export const facultyFilterableFields = [
+  'searchTerm',
+  'id',
+  'gender',
+  'bloodGroup',
+  'email',
+  'contactNo',
+  'emergencyContactNo',
+  'academicFaculty',
+  'academicDepartment',
+  'designation',
+];
+
+export const facultySearchableFields = [
+  'email',
+  'contactNo',
+  'emergencyContactNo',
+  'name.firstName',
+  'name.lastName',
+  'name.middleName',
+];
+
+export const EVENT_FACULTY_UPDATED = 'faculty.updated'
